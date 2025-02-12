237b4ee5a1383cc204f4f9e32c467ca4a21f3309
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2025-02-12 11:49:17 +0100

    Added redux

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -13,10 +13,21 @@ function Order() {
     return <CartItem item={item} key={item.id} />;
   });
 
+  function sum() {
+    const totalSum = cart.reduce((total, item) => {
+      return item.price + total;
+    }, 0);
+
+    return Math.round(totalSum);
+  }
+
   return (
     <section className="order">
       <h2>Cart</h2>
-      <section className="order__summary">{cartItemComponents}</section>
+      <section className="order__summary">
+        {cartItemComponents}
+        <p>Summa: {sum()}</p>
+      </section>
       <Link to="/">Tillbaka till produkter</Link>
     </section>
   );
